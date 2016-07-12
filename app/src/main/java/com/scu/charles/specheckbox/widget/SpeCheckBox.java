a418081edfb03a73a4e86d2e2d2cb5d276be8751
Greatkingcj <331762692@qq.com>
2016-07-12 19:58:49 +0800

    Initial commit

---
+++ app/src/main/java/com/scu/charles/specheckbox/widget/SpeCheckBox.java
@@ -0,0 +1,22 @@
+package com.scu.charles.specheckbox.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by charles on 2016/7/12.
+ */
+public class SpeCheckBox extends LinearLayout{
+    public SpeCheckBox(Context context) {
+        super(context);
+    }
+
+    public SpeCheckBox(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SpeCheckBox(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
